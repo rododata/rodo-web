bc989b8e8e9acaae2e83c726ac2bdcf7e9764a1d
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-10-16 03:16:15 -0300

    Custom queries implementation

--- src/lib/Rododata.ts
+++ src/lib/Rododata.ts
@@ -1,6 +1,8 @@
 import Axios from 'axios';
 import type { ChartType } from 'chart.js';
 
+import { queryableFields } from './store';
+
 const RODODATA_API_URL = import.meta.env['VITE_RODODATA_API_URL'];
 
 const axios = Axios.create({
@@ -14,11 +16,26 @@ export type Dataset = {
     label: string;
 };
 
+export const FilterTypes = ['filterBy', 'groupBy'] as const;
+export type PossibleFilters = typeof FilterTypes[number];
+
+export type FilterableField = {
+    type: PossibleFilters;
+    fieldId: number;
+    name: string;
+};
+
+export const FilterTypeLabel: Record<FilterableField["type"], string> = {
+    filterBy: "Filtrado por",
+    groupBy: "Agrupado por",
+};
+
 export type Card = {
     id: number;
     name: string;
     type?: ChartType;
     datasets: Dataset[];
+    filters: FilterableField[];
 };
 
 export type Dashboard = {
@@ -26,6 +43,35 @@ export type Dashboard = {
     name: string;
     cards?: Card[];
 };
+
+export type QueryField = {
+    id: number;
+    name: string;
+    type: string;
+};
+
+export type QueryOptions = {
+    fields: QueryField[];
+};
+
+export type QueryResult = {
+    data: unknown[][];
+    labels: string[];
+};
+
+type Query<T extends PossibleFilters> = {
+    type: T;
+    fieldId: number;
+};
+
+export type FilterByQuery = Query<'filterBy'> & {
+    operator?: '>' | '<' | '=';
+    value?: unknown;
+};
+
+export type GroupByQuery = Query<'groupBy'>;
+
+export type QueryType = FilterByQuery | GroupByQuery;
 
 export namespace Rododata {
 
@@ -43,6 +89,28 @@ export namespace Rododata {
 
     export async function getCardData(id: number): Promise<GraphResult> {
         const { data } = await axios.get(`/graphs/${id}`);
+
+        return data;
+    }
+
+    export async function getCardDataWithFilters(id: number, query: QueryType): Promise<GraphResult> {
+        const { data } = await axios.post(`/graphs/filter/${id}`, [query]);
+
+        return data;
+    }
+
+    export async function getQueryableFields(): Promise<QueryField[]> {
+        if (queryableFields.loaded)
+            return queryableFields.value;
+
+        const { data } = await axios.get<QueryOptions>('/query/options');
+        queryableFields.set(data.fields);
+
+        return queryableFields.value;
+    }
+
+    export async function query(filters: QueryType[]) {
+        const { data } = await axios.post<QueryResult>('/query', filters);
 
         return data;
     }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,5 +1,32 @@
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 
-import type { Dashboard } from './Rododata';
+import type { Dashboard, QueryField } from './Rododata';
 
-export const dashboards = writable<Dashboard[]>([]);
+function collection<T>(value: T[] = []) {
+    const loaded = writable(false);
+    const collection = writable<T[]>(value);
+
+    const add = (item: T) => collection
+        .update(items => [item, ...items]);
+    const remove = (index: number) => collection
+        .update(items => items.filter((_, i) => i !== index));
+
+    const set = (value: T[]) => {
+        collection.set(value);
+        loaded.set(!!value.length);
+    };
+
+    return {
+        subscribe: collection.subscribe,
+        add, remove, set,
+        get loaded(): boolean {
+            return get(loaded);
+        },
+        get value(): T[] {
+            return get(collection);
+        },
+    };
+}
+
+export const dashboards = collection<Dashboard>();
+export const queryableFields = collection<QueryField>();

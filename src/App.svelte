5b435d978493c9a63f1e2e39662eaa24071ae5c7
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-10-16 03:14:12 -0300

    Global styling & loading indicator

--- src/App.svelte
+++ src/App.svelte
@@ -10,4 +10,23 @@
     @tailwind base;
     @tailwind components;
     @tailwind utilities;
+
+    button {
+        @apply flex justify-center items-center;
+        @apply h-[36px] px-4 space-x-2;
+        @apply rounded bg-indigo-600;
+        @apply outline-none shadow-md;
+        @apply text-white font-medium;
+    }
+
+    input,
+    select {
+        @apply h-[36px] w-full;
+        @apply rounded border border-neutral-400;
+        @apply outline-none;
+    }
+
+    i {
+        @apply select-none;
+    }
 </style>
---
+++ src/components/ProgressCircular.svelte
@@ -0,0 +1,27 @@
+<svg
+    fill="none"
+    viewBox="0 0 24 24"
+    width="48"
+    height="48"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <circle cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4" />
+    <path
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    />
+</svg>
+
+<style lang="postcss">
+    svg {
+        @apply animate-spin text-indigo-600;
+
+        > circle {
+            @apply opacity-25;
+        }
+
+        > path {
+            @apply opacity-75;
+        }
+    }
+</style>
